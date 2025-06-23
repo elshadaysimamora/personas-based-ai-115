04c006f1f0aedb6cd1a97f37a1e529ab1359e1fa
elshadaysimamora <elsasimamora077@gmail.com>
2025-06-23 13:47:34 +0700

    update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Page Title' }}</title>
-    <link rel="icon" href="data:,">
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
 
     <!-- Fonts -->
